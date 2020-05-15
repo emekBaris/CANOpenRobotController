48f0abe95d5fa3dccfd1f805af7957e0d1a298bb
emekB <baris.kucuktabak@gmail.com>
2020-05-15 18:24:16 -0500

    initializing for can0(not using beaglebone not virtualcan)

---
+++ initRobot/initCAN0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#initialisation of Virtual CAN interface and CANopen nodes for 
+#testing of socket connections.
+
+#setting up CAN0
+sudo ip link set can0 up type can bitrate 1000000
+sudo ifconfig can0 up
